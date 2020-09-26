6a8b89c5f00ef443d1a4703b3b51740892bc6270
@erick <erick.hx22@gmail.com>
2020-09-25 23:07:10 -0500

    :recycle: refactor: move app component to pages folder (#2)

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../assets/App.css";
+import { LanguageCard } from "../components/Cards";
+import { LogoHeader } from "../components/Logo";
+const allLanguages = [
+  {
+    name: "English",
+    language: "English",
+  },
+  {
+    name: "Spanish",
+    language: "Español",
+  },
+];
+
+const App = () => {
+  return (
+    <>
+      <LogoHeader />
+      <div className="Page">
+        <div className="Page__container">
+          <h2 className="Page__title">Languages</h2>
+          {/* <FormattedMessage id="languages" defaultMessage="Languages" />: */}
+          <p className="Page__subtitle">
+            This repo has the following languages as demo:
+          </p>
+        </div>
+        <div className="Page__grid">
+          {allLanguages.map((lang) => (
+            <LanguageCard name={lang.name} language={lang.language} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
