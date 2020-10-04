f73ef3a66f42b8731db8cd2ebe72250bb0d2e8c1
@erick <erick.hx22@gmail.com>
2020-10-03 23:22:40 -0500

    :heavy_plus_sign: feat: implement intl provider for i18n (#5)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
+import MessagesEs from "@i18n/translation.json";
 import App from "@pages";
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+enum ELanguage {
+  es = "es",
+  es_ES = "es_ES",
+}
+
+const language = {
+  language: ELanguage.es,
+  languageCode: ELanguage.es_ES,
+};
 
 ReactDOM.render(
-  // <IntlProvider locale={currentLocale} messages={messages}>
-  <App />,
-  // </IntlProvider>
+  <IntlProvider
+    locale={language.language}
+    messages={MessagesEs[language.languageCode]}
+  >
+    <App />
+  </IntlProvider>,
   document.getElementById("root")
 );
